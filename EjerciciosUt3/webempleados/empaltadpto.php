7e81539e9f8543686fe643ce073bd49d789aa06e
pengyu300 <pengyu.ye04@gmail.com>
2025-11-26 08:51:48 +0100

    Create empaltadpto.php

---
+++ EjerciciosUt3/webempleados/empaltadpto.php
@@ -0,0 +1,82 @@
+<!-- El código del departamento tendrá el formato DxxxN (‘D001’, ‘D002’ …) y se
+obtendrá automáticamente.-->
+
+<?php
+
+$servername = "localhost";
+$username   = "root";
+$password   = "rootroot";
+$dbname     = "empleados";
+
+function limpiar_campos($data) {
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    try {
+        $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+        // Lanzar excepciones ante errores
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        // Limpiar nombre
+        $nombre = strtoupper(limpiar_campos($_POST["nombre"]));
+
+        // Iniciar transacción
+        $conn->beginTransaction();
+
+        // Calcular siguiente código
+        $stmt = $conn->prepare(
+            "select cod_dpto from departamento
+             order by cod_dpto desc
+             limit 1"
+        );
+        $stmt->execute();
+        $stmt->setFetchMode(PDO::FETCH_ASSOC);
+        $ultimo = $stmt->fetch();
+
+        if ($ultimo) {
+            $num = intval(substr($ultimo["cod_dpto"], 1)) + 1;
+        }else{
+            $num = 1;
+        }
+
+        // Crear nuevo id con formato DxxxN
+        $cod_dpto = "D" . str_pad($num, 3, "0", STR_PAD_LEFT);
+
+        // Insertar
+        $stmt = $conn->prepare("
+            INSERT INTO departamento (cod_dpto, nombre_dpto)
+            VALUES (:cod_dpto, :nombre)
+        ");
+
+        $stmt->bindParam(':cod_dpto', $cod_dpto);
+        $stmt->bindParam(':nombre', $nombre);
+
+        $stmt->execute();
+
+        // Confirmar transacción
+        $conn->commit();
+
+        echo "<p>Departamento creado:</p>";
+        echo "<p>Código: <strong>$cod_dpto</strong><br>Nombre: <strong>$nombre</strong></p>";
+
+
+    } catch (PDOException $e) {
+
+  
+    echo "Error: " . $e->getMessage() . "<br>";
+
+    // Código de error (SQLSTATE)
+    echo "Código de error: " . $e->getCode() . "<br>";
+
+    
+    }
+
+
+$conn = null;
+}
+
+?>
