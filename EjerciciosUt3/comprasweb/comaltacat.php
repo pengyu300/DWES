89114d1f79223351f1c9bed117b83660e577494d
pengyu300 <pengyu.ye04@gmail.com>
2026-01-16 10:03:32 +0100

    Update comaltacat.php

--- EjerciciosUt3/comprasweb/comaltacat.php
+++ EjerciciosUt3/comprasweb/comaltacat.php
@@ -1,9 +1,10 @@
+<!-- Alta de Categorías (comaltacat.php): dar de alta categorías de productos. -->
 <?php
 
 $servername = "localhost";
 $username = "root";
 $password = "rootroot";
-$dbname = "comprasweb";
+$dbname = "COMPRASWEB";
 
 
 function limpiar_campos($data) {
@@ -13,47 +14,83 @@ function limpiar_campos($data) {
   return $data;
 }
 
+// Crear conexión
+function conectarBD($servername, $username, $password, $dbname){
+    try {
+        // La sentencia de conexión
+        $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+        // Configurar atributos de PDO para manejar errores
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        
+        return $conn;
+        
+    } catch (PDOException $e) {
+
+        echo "Error: " . $e->getMessage();
+        // Terminar la ejecución del script si la conexión falla
+        exit(); 
+    }
+}
+
+// Buscar el ultimo id_categoria existente
+function buscarID($conn){
+  // Buscar el último ID_categoria existente (el mas alto)
+  $stmt = $conn->prepare(
+    "SELECT max(id_categoria) as ultimo 
+     from categoria"
+  );
+
+  $stmt->execute();
+
+  $stmt->setFetchMode(PDO::FETCH_ASSOC);
+  $resultado = $stmt->fetch(); // devuelve solo una fila asociativa
+
+  return $resultado;
+}
+
+//función para insertar la nueva categoria
+function insertarCategoria ($conn, $id_categoria, $nombre){
+  $stmt = $conn->prepare(
+    "INSERT INTO categoria (id_categoria, nombre) 
+     VALUES (:id_categoria, :nombre)");
+
+    $stmt->bindParam(':id_categoria', $id_categoria);
+    $stmt->bindParam(':nombre', $nombre);
+
+    $stmt->execute();
+}
+
+
 try {
     // Crear conexion
-    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-    // Activar modo de errores con excepciones
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $conn = conectarBD($servername, $username, $password, $dbname);
 
     // si el formulario se ha enviado
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $nombre = limpiar_campos($_POST['nombre']);
 
         // Buscar el último ID_categoria existente (el mas alto)
-        $stmt = $conn->prepare("select max(id_categoria) as ultimo from categoria");
-        $stmt->execute();
-        $stmt->setFetchMode(PDO::FETCH_ASSOC);
-        $resultado = $stmt->fetch(); // devuelve solo una fila asociativa
-
+        $buscar = buscarID($conn);
 
         // calcular el siguiente numero
         $num = 1; // si no hay categorias, empezar por 1
-        if ($resultado["ultimo"]){
+        if ($buscar["ultimo"]){
             // Quitar "C-" del ID y convertir a número  Ej: C-003 -> 3
-            $num = intval(substr($resultado["ultimo"], 2))+1;
+            $num = intval(substr($buscar["ultimo"], 2))+1;
         }
 
         // Crear el nuevo id con el formato C-xxx
         // 3: Longitud total deseada, 0: Carácter de relleno, STR_PAD_LEFT: Dirección del relleno
-        $id_nuevo = "C-" . str_pad($num, 3, "0", STR_PAD_LEFT);
+        $id_categoria = "C-" . str_pad($num, 3, "0", STR_PAD_LEFT);
 
         // sentencia para insertar la nueva categoria
-        $stmt = $conn->prepare("INSERT INTO categoria (id_categoria, nombre) VALUES (:id, :nombre)");
-        $stmt->bindParam(':id', $id_nuevo);
-        $stmt->bindParam(':nombre', $nombre);
-        $stmt->execute();
+        insertarCategoria($conn, $id_categoria, $nombre);
 
         // Mostrar mensaje
-        echo "<p>Categoría $nombre creada con ID <strong>$id_nuevo</strong></p>";
-
+        echo "<p>Categoría $nombre creada con ID <strong>$id_categoria</strong></p>";
     }
 
 }
-
 // Si ocurre un error de conexión o SQL, se muestra mensaje
 catch (PDOException $e) {
     echo "Error: " . $e->getMessage();
